0fb886815b3ef1ea5a7677640235f91e2ebf73c9
Subhajit Kar <subhajitkar19@gmail.com>
2025-11-01 15:31:38 +0530

    updates

--- run_julia.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Helper script to run Julia with the correct depot path
-# This avoids OpenSSL_jll and other package depot issues
-
-JULIA_DEPOT_PATH=".julia_depot:$HOME/.julia" julia --project=. "$@"
