8479f0a597542b0564b2b1531b6ea21b21707ad9
Subhajit Kar <subhajitkar19@gmail.com>
2026-05-03 16:32:03 +0300

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,7 @@
 using Test
 include(joinpath(@__DIR__, "precompile_syntax.jl"))
 include(joinpath(@__DIR__, "allocation_static_checks.jl"))
+include(joinpath(@__DIR__, "loop_macro_static_checks.jl"))
 using GeoDynamo
 
 const TEST_DIR = @__DIR__
