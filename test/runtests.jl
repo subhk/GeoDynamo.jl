34a3232a8a44443d162ecb2e672fc03e66c63c28
Subhajit Kar <subhajitkar19@gmail.com>
2026-05-03 18:16:09 +0300

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,6 +7,7 @@ include(joinpath(@__DIR__, "io_writer_static_checks.jl"))
 include(joinpath(@__DIR__, "velocity_boundary_static_checks.jl"))
 include(joinpath(@__DIR__, "magnetic_boundary_static_checks.jl"))
 include(joinpath(@__DIR__, "temperature_boundary_static_checks.jl"))
+include(joinpath(@__DIR__, "composition_boundary_static_checks.jl"))
 using GeoDynamo
 
 const TEST_DIR = @__DIR__
